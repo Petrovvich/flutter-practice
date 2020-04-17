19aad0271abf0593a8487912c3d5c182af86a188
Alexander Romaniuk <a.romanyuk@lantan.info>
2020-04-17 20:03:51 +0700

    finished first app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(),
-));
+void main() => runApp(MaterialApp(home: App()));
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Center(
+          child: Text('I am rich'),
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(40.0),
+          child: Image(
+            image: AssetImage('assets/images/cat.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
