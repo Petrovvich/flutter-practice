9f19c40999ef43e735f5855bed284b8c99d35867
Alexander Romaniuk <a.romanyuk@lantan.info>
2020-04-18 14:13:02 +0700

    finished first challenge

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'challenge.dart';
+
 void main() => runApp(MaterialApp(
+      routes: {'/challenge': (context) => Challenge()},
       home: IAmPoorApp(),
     ));
 
@@ -17,13 +20,24 @@ class _IAmPoorAppState extends State<IAmPoorApp> {
           centerTitle: true,
           backgroundColor: Colors.blue[600],
           title: Text('I am poor')),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(25.0),
-          child: Image(
-            image: AssetImage('assets/images/main_img.jpg'),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Padding(
+              padding: EdgeInsets.all(25.0),
+              child: Image(
+                image: AssetImage('assets/images/main_img.jpg'),
+              ),
+            ),
           ),
-        ),
+          RaisedButton(
+            onPressed: () => Navigator.pushNamed(context, '/challenge'),
+            child: Text(
+              'Go to challenge',
+              style: TextStyle(color: Colors.blue),
+            ),
+          )
+        ],
       ),
       backgroundColor: Colors.grey[200],
     );
