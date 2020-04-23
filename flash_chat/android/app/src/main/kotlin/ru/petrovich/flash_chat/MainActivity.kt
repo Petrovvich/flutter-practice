6ca5f3d8c58dea726e748e0fffe8d0474c7fe04f
Alexander Romaniuk <a.romanyuk@lantan.info>
2020-04-23 09:46:11 +0700

    fix firebase integration

--- flash_chat/android/app/src/main/kotlin/ru/petrovich/flash_chat/MainActivity.kt
+++ flash_chat/android/app/src/main/kotlin/ru/petrovich/flash_chat/MainActivity.kt
@@ -1,6 +1,6 @@
 package ru.petrovich.flash_chat
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
